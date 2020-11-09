89b083cb91b602af1b5be93dc7765fe8208a0709
Pierre Ugaz <ulm0@innersea.xyz>
2020-11-09 15:21:16 -0300

    chore: gatekeeper addon

--- vars.tf
+++ vars.tf
@@ -92,7 +92,7 @@ variable upgrade_max_unavailable_worker { default = "10%" }
 variable write_kubeconfig { default = true }
 
 # addons
-variable addons { default = "cert-manager,descheduler,dex,externaldns,gangway,ingress,klum,kured,metallb" }
+variable addons { default = "cert-manager,descheduler,dex,externaldns,gangway,gatekeeper,ingress,klum,kured,metallb" }
 
 # ingress
 variable ingress_autoscale { default = true }
